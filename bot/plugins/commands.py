c813e2923fabf3905267777211f4353b783fc126
Johndalton7 <86659836+Johndalton7@users.noreply.github.com>
2021-07-01 12:33:47 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'JOIN CHANNEL ✔️', url="https://t.me/MFMOVIES3"
                                 )
                         ]
                     ]
@@ -57,7 +57,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'JOIN CHANNEL ✔️', url="https://t.me/MFMOVIES3"
                                 )
                         ]
                     ]
@@ -89,10 +89,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('🙅 CREATOR', url='https://t.me/JohnDalton_TG'),
+        InlineKeyboardButton('GROUP', url ='https://t.me/MF_CHATGROUP')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('CHANNEL', url='https://t.me/MFMOVIES3')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
